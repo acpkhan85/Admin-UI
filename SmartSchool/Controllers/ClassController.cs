03278de803560b82176d3f88aae374afc7f22378
acpkhan85 <acpkhan85@gmail.com>
2017-07-23 20:32:59 +0530

    Class and division setup

---
+++ SmartSchool/Controllers/ClassController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartSchool.Controllers
+{
+    public class ClassController : Controller
+    {
+        // GET: Class
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Division()
+        {
+            return View();
+        }
+    }
+}
---
+++ SmartSchool/Controllers/NoticeBoardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartSchool.Controllers
+{
+    public class NoticeBoardController : Controller
+    {
+        // GET: NoticeBoard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
