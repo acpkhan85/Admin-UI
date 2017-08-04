c33487c45bdf1d21379fdf6ba783a1afee165e19
acpkhan85 <acpkhan85@gmail.com>
2017-08-05 00:07:11 +0530

    register page

---
+++ SmartSchool/Controllers/AttendanceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartSchool.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        // GET: Attendance
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
