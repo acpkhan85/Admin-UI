176ac1dc346d3b337d660c24625f0fd8ef5aa00b
acpkhan85 <acpkhan85@gmail.com>
2017-08-13 01:12:59 +0530

    consume wcf service

--- SmartSchool/Controllers/EventsController.cs
+++ SmartSchool/Controllers/EventsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Contract.SmartSchool;
+using SmartSchool.Helper;
 
 namespace SmartSchool.Controllers
 {
@@ -11,6 +13,11 @@ namespace SmartSchool.Controllers
         // GET: Events
         public ActionResult Index()
         {
+            WCFProxy.Using((delegate (IClassSetupService client)
+            {
+                var resulrt = client.getExamTimeTable(1);
+            }));
+
             return View();
         }
     }
---
+++ SmartSchool/Helper/ProxyHelper.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace SmartSchool.Helper
+{     
+    public class WCFProxy
+    {
+        public static void Using<T>(Action<T> action)
+        {
+            BasicHttpBinding bindnig = new BasicHttpBinding();
+            string endpointUri = GetServiceEndpoint(typeof(T));
+            ChannelFactory<T> factory = new ChannelFactory<T>(bindnig, new EndpointAddress(new Uri(endpointUri)));
+
+            //ChannelFactory<T> factory = new ChannelFactory<T>("*");
+
+            T client = factory.CreateChannel();
+
+            try
+            {
+                action(client);
+                ((IClientChannel)client).Close();
+                factory.Close();
+            }
+            catch (Exception ex)
+            {
+                IClientChannel clientInstance = ((IClientChannel)client);
+                if (clientInstance.State == System.ServiceModel.CommunicationState.Faulted)
+
+                {
+                    clientInstance.Abort();
+                    factory.Abort();
+                }
+                else if (clientInstance.State != System.ServiceModel.CommunicationState.Closed)
+                {
+                    clientInstance.Close();
+                    factory.Close();
+                }
+                throw (ex);
+            }
+        }
+        private static string GetServiceEndpoint(Type service)
+        {
+
+            string serviceTypeName = service.Name;
+            //We will configure this from Config file
+            switch (serviceTypeName)
+            {
+                case "IUserService":
+                    serviceTypeName = "http://localhost:17219/UserService.svc";
+                    break;
+                case "IEventsAndNewsService":
+                    serviceTypeName = "http://localhost:17219/EventsAndNewsService.svc";
+                    break;
+                case "IClassSetupService":
+                    serviceTypeName = "http://localhost:17219/ClassSetupService.svc";
+                    break;
+                case "IAboutService":
+                    serviceTypeName = "http://localhost:17219/AboutService.svc";
+                    break;
+            }
+
+            return serviceTypeName;
+
+        }
+    }
+}
